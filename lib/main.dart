db6d225bb5153beac8372eae08c036c80e9dec33
FLOpzt <pauzatflorian@gmail.com>
2022-07-26 14:52:03 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('DéDé'),
+        backgroundColor: Colors.red,
+      ),
+      body: DiceGame(),
+    ),
+  ));
+}
+
+class DiceGame extends StatefulWidget {
+  @override
+  State<DiceGame> createState() => _DiceGameState();
+}
+
+class _DiceGameState extends State<DiceGame> {
+  int leftdice = 1;
+  int rightdice = 2;
+
+  rolldices() {
+    setState(() {
+      leftdice = Random().nextInt(6) + 1;
+      rightdice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                rolldices();
+              },
+              child: Image.asset('assets/images/dice$leftdice.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                rolldices();
+              },
+              child: Image.asset('assets/images/dice$rightdice.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
